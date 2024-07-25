2e241c732d2a3565e7cc0afce4c8c3ee31ea2254
ra-boc <54794951+ra-boc@users.noreply.github.com>
2024-07-25 11:39:34 +0900

    Update apikey.hpp

--- gptapp/apikey.hpp
+++ gptapp/apikey.hpp
@@ -2,6 +2,6 @@
 class apikey
 {
 	public:
-		const inline static String getapikey(){ return U"sk-proj-v3MkWoYqby8LIGInRdgOT3BlbkFJTDvttkav8277RaIPOU5c"; }
+		const inline static String getapikey(){ return U"API_KEY"; }
 };
 
